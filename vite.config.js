496a202a5a2ae28a06a62768d91418e82cb3994a
codeming <hmk2000015@gmail.com>
2025-04-18 02:03:19 +0900

    chore: 배포용 설정

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  base: '/front-5th-chapter2-1/',
+  build: {
+    outDir: 'docs',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: 'src/setupTests.js'
+    setupFiles: 'src/setupTests.js',
   },
-})
+});
