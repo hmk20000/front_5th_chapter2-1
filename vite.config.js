c407356559878ced4266f5c1c0655a76b6894aeb
codeming <hmk2000015@gmail.com>
2025-04-18 02:07:21 +0900

    fix: 배포용 경로 오류 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  base: '/front-5th-chapter2-1/',
+  base: '/front_5th_chapter2-1/',
   build: {
     outDir: 'docs',
   },
